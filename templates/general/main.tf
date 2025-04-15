f8835856decf21e6e3ca19255d0d27092fc38720
Pawel Chylak <p.chylak@it-spec.eu>
2025-04-15 12:16:51 +0200

    fix db module

--- templates/general/main.tf
+++ templates/general/main.tf
@@ -96,7 +96,7 @@ module "mysql_flexible_server" {
 resource "azurerm_mysql_flexible_database" "wordpressdb" {
   name                = "wordpressdb"
   resource_group_name = azurerm_resource_group.this.name
-  server_name         = module.mysql_flexible_server.name
+  server_name         = module.mysql_flexible_server.mysql_flexible_server.name
   charset             = "utf8mb4"
   collation           = "utf8mb4_unicode_ci"
 }
