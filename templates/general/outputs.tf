9b39bc49f8826b4805277a90c19949d46de3a67f
Pawel Chylak <pawel.chylak.ext@hemolens.com>
2025-04-24 00:38:25 +0200

    mysql

--- templates/general/outputs.tf
+++ templates/general/outputs.tf
@@ -1,10 +1,10 @@
 output "mysql_flexible_server" {
   value = {
-    name     = azurerm_mysql_flexible_server.this.name
-    id       = azurerm_mysql_flexible_server.this.id
-    host     = format("%s.mysql.database.azure.com", azurerm_mysql_flexible_server.this.name)
-    username = azurerm_mysql_flexible_server.this.administrator_login
-    password = random_password.this.result
+    name     = module.mysql_flexible_server.name
+    id       = module.mysql_flexible_server.id
+    host     = format("%s.mysql.database.azure.com", module.mysql_flexible_server.name)
+    username = module.mysql_flexible_server.administrator_login
+    password = module.mysql_flexible_server.administrator_password
   }
   sensitive = true
 }
