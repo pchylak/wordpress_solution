b35ec9e7f31bdb762b9588d0e52b6288b9f833e2
Pawel Chylak <pawel.chylak.ext@hemolens.com>
2025-04-23 23:43:39 +0200

    add region module

--- templates/wp/main.tf
+++ templates/wp/main.tf
@@ -1,3 +1,10 @@
+module "azure_region" {
+  source  = "claranet/regions/azurerm"
+  version = "7.3.1"
+
+  azure_region = var.region_name
+}
+
 resource "azurecaf_name" "this" {
   resource_types = [
     "azurerm_resource_group",
